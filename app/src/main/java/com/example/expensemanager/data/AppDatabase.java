1966ebc40fc71f59d50e5b4a0c6504240c04d878
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-06-13 21:59:49 +0530

    Create AppDatabase.java

---
+++ app/src/main/java/com/example/expensemanager/data/AppDatabase.java
@@ -0,0 +1,30 @@
+package com.example.expensemanager.data;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Income.class, Expense.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract ExpenseManagerDao expenseManagerDao();
+
+    private static volatile AppDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "expense_manager_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
