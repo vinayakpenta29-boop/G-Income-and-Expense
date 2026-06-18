7b60b2960e5d3259e8160c41aff8aad1071e9803
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-06-18 20:07:37 +0530

    Update TransactionItem.java

--- app/src/main/java/com/example/expensemanager/data/TransactionItem.java
+++ app/src/main/java/com/example/expensemanager/data/TransactionItem.java
@@ -5,16 +5,26 @@ public class TransactionItem {
     private String date;
     private double amount;
     private boolean isIncome;
+    private String note;
+    private String linkedSourceName;
+    private double linkedSourceAvailableBalance;
 
-    public TransactionItem(String title, String date, double amount, boolean isIncome) {
+    public TransactionItem(String title, String date, double amount, boolean isIncome, 
+                           String note, String linkedSourceName, double linkedSourceAvailableBalance) {
         this.title = title;
         this.date = date;
         this.amount = amount;
         this.isIncome = isIncome;
+        this.note = note;
+        this.linkedSourceName = linkedSourceName;
+        this.linkedSourceAvailableBalance = linkedSourceAvailableBalance;
     }
 
     public String getTitle() { return title; }
     public String getDate() { return date; }
     public double getAmount() { return amount; }
     public boolean isIncome() { return isIncome; }
+    public String getNote() { return note; }
+    public String getLinkedSourceName() { return linkedSourceName; }
+    public double getLinkedSourceAvailableBalance() { return linkedSourceAvailableBalance; }
 }
